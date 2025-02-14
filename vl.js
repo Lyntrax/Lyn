2c8fd20b65b23e10ccb98743de2b57f19a2df90f
Lyntrax <johncarlocaspe4@gmail.com>
2025-02-14 07:00:41 +0400

    Add files via upload

---
+++ vl.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    var balloonCount = 0;
+    var maxBalloons = 7;
+
+    $('.container').mouseenter(function () {
+        if (balloonCount < maxBalloons) {
+            var balloon = $('<div class="balloon"><div class="heart-balloon"><div class="balloon-text">I love you</div></div></div>');
+            balloon.appendTo('.balloon-container').fadeIn(500);
+            balloonCount++;
+        }
+        // Animate the card and reveal the heart when the card slides up
+        $('.card').stop().animate({
+            top: '-90px'
+        }, 'slow').addClass('open'); // Add 'open' class to trigger visibility
+    }).mouseleave(function () {
+        $('.card').stop().animate({
+            top: 0
+        }, 'slow').removeClass('open'); // Remove 'open' class to hide heart again
+    });
+});
